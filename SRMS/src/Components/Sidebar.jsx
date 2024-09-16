34a9836e481e04110d385f07b14326b8ac6e181e
samayine <samiayine3@gmail.com>
2024-09-16 22:43:37 +0300

    updating sidbar by moving somecode to header.jsx

--- SRMS/src/Components/Sidebar.jsx
+++ SRMS/src/Components/Sidebar.jsx
@@ -1,21 +1,10 @@
 import { Link } from "react-router-dom";
 import "../Styles/MainContent.css";
 import { FaHome, FaClipboard, FaCalendarAlt, FaClock } from "react-icons/fa";
-import SchoolLogo from "../assets/Logo.png";
 
 const Sidebar = () => {
   return (
     <aside className="sidebar w-1/5 bg-white border-r shadow-lg flex flex-col">
-      <div className="sidebar__header p-6 flex flex-col items-center">
-        <div className="sidebar__logo flex items-center">
-          <img
-            src={SchoolLogo}
-            alt="Logo"
-            className="sidebar__logo-image w-10 h-10"
-          />
-          <h1 className="sidebar__title text-teal-600 font-bold ml-3">SRMS</h1>
-        </div>
-      </div>
       <nav className="sidebar__nav flex flex-col flex-grow">
         <ul className="sidebar__list space-y-4 p-6">
           <li className="sidebar__item flex items-center text-purple-600 font-semibold">
