3c91b056a3c469520c9d76ed9eb911302385e4a8
samayine <samiayine3@gmail.com>
2024-09-16 23:50:35 +0300

    changing for into 'htmlFor' inside graderesult

--- SRMS/src/Pages/GradeResult.jsx
+++ SRMS/src/Pages/GradeResult.jsx
@@ -8,7 +8,7 @@ function GradeResult() {
       <div className="flex justify-between items-center mb-4">
         <h2 className="text-2xl font-semibold">Grade Result</h2>
         <div className="flex items-center">
-          <label for="semester" className="mr-2 text-lg font-medium">
+          <label htmlFor="semester" className="mr-2 text-lg font-medium">
             Semester
           </label>
           <select id="semester" className="border rounded px-3 py-1">
